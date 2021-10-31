7d935ae6249d0f078764c124ac30f80c46880c06
abc <abc@xyz.com>
2021-10-31 20:29:31 +0530

    Project guide file modified.

--- ServletSessionCookiesDemo/src/controller/MyServlet.java
+++ ServletSessionCookiesDemo/src/controller/MyServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  * Servlet implementation class MyServlet
  */
